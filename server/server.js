2d2c8f0bf2f8dd1425d163c31673cdf7f72fc369
GamingPrower <dcody12@hotmail.com>
2020-04-19 18:59:56 -0400

    Server setup

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+const transactions = require('./routes/transactions');
+
+const app = express();
+
+app.use(express.json());
+
+// Requests to /api/v1/transactions route to the '/' route
+app.use('/api/v1/transactions', transactions);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`${process.env.NODE_ENV} server listening on Port ${PORT}`.yellow.bold));
