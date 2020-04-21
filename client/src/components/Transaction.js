4079231825c846cc60cef610fa5f7ca21611c807
GamingPrower <dcody12@hotmail.com>
2020-04-20 23:44:38 -0400

    Added conditional rendering and edit functionality

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -1,35 +1,76 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { GlobalContext } from '../context/GlobalState'
 import { numberWithCommas } from '../utils/format';
 
 const Transaction = ({ transaction }) => {
-	const { deleteTransaction } = useContext(GlobalContext);
+	const { deleteTransaction, editTransaction, userId } = useContext(GlobalContext);
+	const [formFocused, setFormFocused] = useState(false);
+	const [values, setValues] = useState({ text: transaction.text, amount: transaction.amount });
 
 	const moneyStyle = {
 		color: transaction.amount < 0 ? 'red' : transaction.amount > 0 ? 'green' : 'black',
 		fontSize: '.75em'
 	};
 
-	return (
-		<>
-			<div className="right floated content">
-				<button
-					onClick={() => deleteTransaction(transaction._id)}
-					className="ui red labeled icon button"
-				>
-				<i className="trash icon"></i>
-				Remove
-				</button>
-			</div>
-			<div className="content">
-				<div className="ui tiny header">
-					{transaction.text}
+	const submitEdit = async () => {
+		await editTransaction({...transaction, text: values.text, amount: +(values.amount)});
+		setFormFocused(false);
+	};
+
+	if (formFocused) {
+		return (
+			<>
+				<div className="content">
+					<div className="ui form">
+						<button
+							onClick={() => setFormFocused(false)}
+							className="ui red icon right floated button"
+						>
+							<i className="close icon"></i>
+						</button>
+						<button
+							type="submit"
+							onClick={submitEdit}
+							className="ui green icon right floated button"
+						>
+							<i className="check icon"></i>
+						</button>
+						
+						<div className="fields">
+							<div className="field">
+								<input type="text" name="text" value={values.text} onChange={(e) => setValues({...values, [e.target.name]: e.target.value})} />
+							</div>
+							<div className="field">
+								<input type="number" step="0.01" name="amount" value={values.amount} onChange={(e) => setValues({...values, [e.target.name]: e.target.value})} />
+							</div>
+						</div>
+					</div>
+					
+					
 				</div>
-				<div style={moneyStyle}>{numberWithCommas(transaction.amount)}</div>
+			</>
+		);
+	} else {
+		return (
+			<div onClick={() => setFormFocused(true)}>
+				<div className="right floated content">
+					<button
+						onClick={() => deleteTransaction(transaction._id)}
+						className="ui red labeled icon button"
+					>
+					<i className="trash icon"></i>
+					Remove
+					</button>
+				</div>
+				<div className="content">
+					<div className="ui tiny header">
+						{transaction.text}
+					</div>
+					<div style={moneyStyle}>{numberWithCommas(transaction.amount)}</div>
+				</div>
 			</div>
-			
-		</>
-	)
+		)
+	}
 }
 
 export default Transaction
