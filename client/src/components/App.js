c0e93dab8d86f27e3ff9078ae78cb2bc3e05371d
GamingPrower <dcody12@hotmail.com>
2020-04-20 23:43:22 -0400

    Moved components to ViewController

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import Header from './Header'
-import Balance from './Balance'
-import IncomeExpense from './IncomeExpense'
-import TransactionList from './TransactionList'
-import AddTransaction from './AddTransaction'
+import ViewController from './ViewController'
 import { GlobalProvider } from '../context/GlobalState'
 
 const centerStyle = {
@@ -19,11 +15,7 @@ const App = () => {
 		<GlobalProvider>
 			<div style={centerStyle}>
 				<div className="ui raised very padded text container segment">
-					<Header />
-					<Balance />
-					<IncomeExpense />
-					<TransactionList />
-					<AddTransaction />
+					<ViewController />
 				</div>
 			</div>
 			
